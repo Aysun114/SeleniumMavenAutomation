b38428736a647975de3f2e1394dfde819e09c92c
Aysun114 <aysun114@hotmail.com>
2018-07-13 19:53:35 -0500

    print test and timestamp

--- src/test/java/com/dice/DiceJobSearch.java
+++ src/test/java/com/dice/DiceJobSearch.java
@@ -1,5 +1,6 @@
 package com.dice;
 
+import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -63,6 +64,7 @@ public class DiceJobSearch {
 					countResult+" results in "+ location);	
 			}
 		driver.close();
+		System.out.println("Test Completed- "+ LocalDateTime.now());
 	}
 		
 	}
